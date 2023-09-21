6d305d8fe9e3e0e476a749d90558ebc37b0d36a3
@dinhhuuphucthinh2002 <dinhhuuphucthinh2002@gmail.com>
2023-09-21 16:55:42 +0700

    ver3

--- src/components/header.js
+++ src/components/header.js
@@ -6,14 +6,14 @@ export default class Header extends Component {
     return (
       <div className='header'>
         <nav>
-          <ul>
+          <ul className='header-menu'>
             <img className='logo' src='https://cc-prod.scene7.com/is/image/CCProdAuthor/mascot-logo-design_P1_900x420?$pjpeg$&jpegSize=200&wid=900' alt='app xem phim'/>
-          <li> <a className='active' href='#home'>Home </a> </li>
-          <li> <a href='#new'> New</a></li>
-          <li> <a href='#about'> About</a></li>
-          <li> <a href='#contact'> Contact</a></li>
-           <form>
-            <input type='text' placeholder='Tìm tên phim...' name='keyword' />
+          <li className='home'> <a className='active' href='#home'>Home </a> </li>
+          <li className='new'> <a href='#new'> New</a></li>
+          <li className='about'> <a href='#about'> About</a></li>
+          <li className='contact'> <a href='#contact'> Contact</a></li>
+           <form className='search-bar'>
+            <input  className='search' type='text' placeholder='Tìm tên phim...' name='keyword' />
             <FontAwesomeIcon icon={faMagnifyingGlass} />
             </form>
           </ul>
--- src/components/recommend.js
+++ src/components/recommend.js
@@ -1,18 +1,19 @@
 import React from "react";
+import "./recommend.css"
 
 export default function Recommend({ film }) {
     return (
         <div className="recommend">
-            <p className="recommend-tilte"> Phim đề cử</p>
-            <div className="container">
-                {
-                    film.map((RecommendFilm) => (
-                        <div className="recommend-film" >
+            <h2 className='recommend-tilte'>Phim Đề Cử</h2>
+            <div className="recommend-container">
+                {                  
+                    film.map((RecommendFilm) => (                                             
+                        <div className="recommend-film" key={RecommendFilm.id} >
                             <div className="recommend-card">
-                                <div className="img">
-                                    <img src={RecommendFilm.img} />
-                                    <h4>{RecommendFilm.tilte}</h4>
+                                <div className="recommend-img">
+                                    <img src={RecommendFilm.img} />                                  
                                 </div>
+                                <h4 className="recommend-name">{RecommendFilm.tilte}</h4>
                             </div>
                         </div>
                     )
