e3faa32f6c29a593fd78769e857ab99348924dc0
@dinhhuuphucthinh2002 <dinhhuuphucthinh2002@gmail.com>
2023-09-22 16:50:11 +0700

    ver4

--- src/components/recommend.js
+++ src/components/recommend.js
@@ -6,13 +6,11 @@ export default function Recommend({ film }) {
         <div className="recommend">
             <h2 className='recommend-tilte'>Phim Đề Cử</h2>
             <div className="recommend-container">
-                {                  
-                    film.map((RecommendFilm) => (                                             
+                {
+                    film.map((RecommendFilm) => (
                         <div className="recommend-film" key={RecommendFilm.id} >
                             <div className="recommend-card">
-                                <div className="recommend-img">
-                                    <img src={RecommendFilm.img} />                                  
-                                </div>
+                                <img className="recommend-img" src={RecommendFilm.img} />
                                 <h4 className="recommend-name">{RecommendFilm.tilte}</h4>
                             </div>
                         </div>
