39ec9fd9174367268aa5b6cea6cdf24ab33550dd
@dinhhuuphucthinh2002 <dinhhuuphucthinh2002@gmail.com>
2023-09-23 14:19:34 +0700

    ver6

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 export default class Header extends Component {
   render() {
@@ -7,16 +7,16 @@ export default class Header extends Component {
       <div className='header'>
         <nav>
           <ul className='header-menu'>
-            <img className='logo' src='https://cc-prod.scene7.com/is/image/CCProdAuthor/mascot-logo-design_P1_900x420?$pjpeg$&jpegSize=200&wid=900' alt='app xem phim'/>
-          <li className='home'> <a className='active' href='#home'>Home </a> </li>
-          <li className='new'> <a href='#new'> New</a></li>
-          <li className='about'> <a href='#about'> About</a></li>
-          <li className='contact'> <a href='#contact'> Contact</a></li>
-           <form className='search-bar'>
-            <input  className='search' type='text' placeholder='Tìm tên phim...' name='keyword' />
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-            </form>
+            <img className='logo' src='https://cc-prod.scene7.com/is/image/CCProdAuthor/mascot-logo-design_P1_900x420?$pjpeg$&jpegSize=200&wid=900' alt='app xem phim' />
+            <li className='home'> <a className='active' href='#home'>Home </a> </li>
+            <li className='new'> <a href='#new'> New</a></li>
+            <li className='about'> <a href='#about'> About</a></li>
+            <li className='contact'> <a href='#contact'> Contact</a></li>
           </ul>
+          <form className='search-bar'>
+            <input className='search' type='text' placeholder='Tìm tên phim...' name='keyword' />
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </form>
         </nav>
       </div>
     )
