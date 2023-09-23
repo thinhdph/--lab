3c131f63ec9bdbd87e038cba8a7f0697dd1698f5
@dinhhuuphucthinh2002 <dinhhuuphucthinh2002@gmail.com>
2023-09-23 13:45:24 +0700

    ver5

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Header from './components/header';
 import  Footer  from './components/footer';
 import Main from './components/main';
-import Recommend from './components/recommend';
 
 function App() {
   return (
--- src/components/content.js
+++ src/components/content.js
@@ -1,28 +1,11 @@
 import React from "react";
+import { useState } from "react";
+import './content.css'
 function Detail() {
     alert('message');
 }
-//  export default function Content() {
-//     // const Detail = () => alert('aa')
-//         return (
-
-//         <div className='container'>
-//         {
-//             Filmlist.map((film) => (
-//                 <div className='column' key ={film.id} >
-//                     <div className='card' >
-//                         <img  className='img-content' src={film.img} />
-//                         <h3 >{film.tilte}</h3>
-//                         <p className='year'> {film.year}</p>
-//                         <p className='nation'> {film.nation}</p>
-//                         {<p><button onClick={Detail}>Detail</button></p> }
-//                     </div>
-//                 </div>
-//             ))
-//         }</div>
-//     )
-// }
 export default function Content({ films }) {
+    const [film,setFilm]=useState([])
     // const Detail = () => alert('aa')
     return (
         <div className="content">
@@ -39,11 +22,22 @@ export default function Content({ films }) {
                                 <h3 >{film.tilte}</h3>
                                 <p className='year'> {film.year}</p>
                                 <p className='nation'> {film.nation}</p>
-                                {<p><button onClick={Detail}>Detail</button></p>}
+                                {<p><a href='#popup' id='showPopUp'><button onClick={()=>{setFilm(film)}}>Detail</button></a></p>}
                             </div>
                         </div>
+                        
                     ))
                 }</div>
+                <div id="popup" className="overlay">
+                    <div className="popup">
+                        <img className="popup-img" src={film.img}/>
+                        <h2>{film.tilte}</h2>
+                        <a className="close" href="#">X</a>
+                        <div className="info">
+                            {film.info}
+                        </div>
+                    </div>
+                </div>
         </div>
 
     )
