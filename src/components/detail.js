f7e3c47d7b74dbbbbd73de6083dbe850684fe57d
@dinhhuuphucthinh2002 <dinhhuuphucthinh2002@gmail.com>
2023-10-05 21:59:35 +0700

    them video

--- src/components/detail.js
+++ src/components/detail.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
 import { useParams } from 'react-router-dom'
 import { Filmlist } from "./object/Filmlist";
 export default function Detail() {  
